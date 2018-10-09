44475f8bbae1038a54612eccacb224afcce649d7
junge8618 <513851613@qq.com>
2018-10-09 22:15:33 +0800

    初始化文件

---
+++ springboot-email/src/main/java/com/junge/demo/springboot/email/SpringbootEmailApplication.java
@@ -0,0 +1,12 @@
+package com.junge.demo.springboot.email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEmailApplication.class, args);
+	}
+}
