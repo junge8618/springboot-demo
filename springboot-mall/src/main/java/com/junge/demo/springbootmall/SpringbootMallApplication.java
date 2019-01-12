84caf9a7d4d80dc2442286d213565663e2e6eb6c
junge8618 <513851613@qq.com>
2019-01-12 08:04:54 +0800

    初始化工程

---
+++ springboot-mall/src/main/java/com/junge/demo/springbootmall/SpringbootMallApplication.java
@@ -0,0 +1,14 @@
+package com.junge.demo.springbootmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMallApplication.class, args);
+	}
+
+}
+
